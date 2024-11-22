7ab4653d22e684fe02b086806c6568b2f247a2b1
hang.lethibich <21522041@gm.uit.edu.vn>
2024-11-22 08:16:17 +0700

    Change the api base url

--- src/services/camera.service.ts
+++ src/services/camera.service.ts
@@ -1,8 +1,11 @@
 import { Camera } from "../utils/types";
 
-const API_BASE_URL = 'http://localhost:8004/api/v1/cameras';
+const API_BASE_URL = "https://camera-service.onrender.com/api/v1/cameras";
 
-export const getListCameraService = async ({ isEnabled, search }: { isEnabled?: boolean, search?: string} = {}): Promise<Camera[]> => {
+export const getListCameraService = async ({
+  isEnabled,
+  search,
+}: { isEnabled?: boolean; search?: string } = {}): Promise<Camera[]> => {
   const url = new URL(`${API_BASE_URL}/cameras`);
 
   if (isEnabled !== undefined) {
@@ -20,23 +23,31 @@ export const getListCameraService = async ({ isEnabled, search }: { isEnabled?: 
   return await response.json();
 };
 
-export const getCameraByIdService = async (cameraId: string): Promise<Camera> => {
-    const response = await fetch(`${API_BASE_URL}/cameras/${cameraId}`);
-    if (!response.ok) {
-      throw new Error('Failed to fetch camera by ID');
-    }
-    return await response.json();
+export const getCameraByIdService = async (
+  cameraId: string
+): Promise<Camera> => {
+  const response = await fetch(`${API_BASE_URL}/cameras/${cameraId}`);
+  if (!response.ok) {
+    throw new Error("Failed to fetch camera by ID");
   }
+  return await response.json();
+};
 
-export const updateCameraStatusService = async (camera_id_list: string[], is_enabled: boolean) => {
+export const updateCameraStatusService = async (
+  camera_id_list: string[],
+  is_enabled: boolean
+) => {
   try {
-    const response = await fetch(`http://localhost:8004/api/v1/cameras/cameras/status?is_enabled=${is_enabled}`, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(camera_id_list),
-    });
+    const response = await fetch(
+      `${API_BASE_URL}/cameras/status?is_enabled=${is_enabled}`,
+      {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(camera_id_list),
+      }
+    );
 
     if (!response.ok) {
       throw new Error(`Error updating cameras: ${response.statusText}`);
